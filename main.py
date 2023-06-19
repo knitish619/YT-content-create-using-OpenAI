5f97063aed94943b159b3118307ee513ed0bdf89
Kanta Nithish <104885916+knitish619@users.noreply.github.com>
2023-06-19 08:23:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from langchain.utilities import WikipediaAPIWrapper
 os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
-st.title('🦜🔗 YouTube GPT Creator')
+st.title(' YouTube Creator')
 prompt = st.text_input('Plug in your prompt here') 
 
 # Prompt templates
@@ -21,8 +21,8 @@ title_template = PromptTemplate(
 )
 
 script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'], 
-    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
+    input_variables = ['title'], 
+    template='write me a youtube video script based on this title TITLE: {title}  '
 )
 
 # Memory 
@@ -35,13 +35,12 @@ llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-wiki = WikipediaAPIWrapper()
+
 
 # Show stuff to the screen if there's a prompt
 if prompt: 
     title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
+    script = script_chain.run(title=title)
 
     st.write(title) 
     st.write(script) 
@@ -52,5 +51,3 @@ if prompt:
     with st.expander('Script History'): 
         st.info(script_memory.buffer)
 
-    with st.expander('Wikipedia Research'): 
-        st.info(wiki_research)
